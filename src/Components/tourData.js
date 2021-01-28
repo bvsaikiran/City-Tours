97101e3c1e27a0a80f4280c311262f076b4272bd
saikiran.bandaru <saikiran.bandaru@MQADS.HYD>
2021-01-28 17:23:29 +0530

    Initial Commit

---
+++ src/Components/tourData.js
@@ -0,0 +1,39 @@
+const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "https://images.pexels.com/photos/374870/pexels-photo-374870.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    name: "new york bridge tour",
+    info:
+      "The Brooklyn Bridge is a hybrid cable-stayed/suspension bridge in New York City",
+    ref:"https://en.wikipedia.org/wiki/List_of_bridges_and_tunnels_in_New_York_City"
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "https://images.pexels.com/photos/1121782/pexels-photo-1121782.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    name: "paris lights tour",
+    info:
+      "Paris, city and capital of France, located along the Seine River, in the north-central part of the country.",
+    ref:"https://www.toureiffel.paris/en/the-monument/lights"
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "https://images.pexels.com/photos/936722/pexels-photo-936722.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    name: "london royal palace tour",
+    info:
+      "London is the capital and largest city of England and the United Kingdom.",
+    ref:"https://www.londonhut.com/p/10-royal-palaces-of-london"
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "https://images.pexels.com/photos/1036657/pexels-photo-1036657.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    name: "tokyo sushi tour",
+    info:
+      "Tokyo, city and capital of Tokyo 'to' (metropolis) and of Japan.",
+    ref:"https://www.viator.com/en-IN/Tokyo/d334-ttd"
+  }
+];
+export default tourData
